3545a8cfa925477c9c451f2b9bffd2577b924122
Dimash <dimashabdygalykov@MacBook-Air-Dimash-2.local>
2026-02-08 15:00:06 +0500

    type_conversions

---
+++ lab3/task1/interaction_alert.js
@@ -0,0 +1,11 @@
+// result = prompt(title, [default]);
+
+// It shows a modal window with a text message, an input field for the visitor, and the buttons OK/Cancel.
+
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed

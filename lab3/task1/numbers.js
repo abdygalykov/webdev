2c653c3ae5fa48299329fab8ce383efead2e7d68
Dimash <abdygalykd@gmail.com>
2026-02-09 20:17:58 +0500

    Numbers

---
+++ lab3/task1/numbers.js
@@ -0,0 +1,27 @@
+let billion = 1000000000;
+
+let billion2 = 1_000_000_000;
+
+let billion3 = 1e9;  // 1 billion, literally: 1 and 9 zeroes
+
+alert( 7.3e9 );  // 7.3 billions (same as 7300000000 or 7_300_000_000)
+
+
+let mcs = 1e-6; // five zeroes to the left from 1
+
+let mсs = 0.000001;
+
+alert( Number.isNaN(NaN) ); // true
+alert( Number.isNaN("str" / 2) ); // true
+
+// Note the difference:
+alert( Number.isNaN("str") ); // false, because "str" belongs to the string type, not the number type
+alert( isNaN("str") ); // true, because isNaN converts string "str" into a number and gets NaN as a result of this conversion
+
+alert( Number.isFinite(123) ); // true
+alert( Number.isFinite(Infinity) ); // false
+alert( Number.isFinite(2 / 0) ); // false
+
+// Note the difference:
+alert( Number.isFinite("123") ); // false, because "123" belongs to the string type, not the number type
+alert( isFinite("123") ); // true, because isFinite converts string "123" into a number 123

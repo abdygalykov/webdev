80297a85dcff9a2e71a986edc10719e3f21dbb2b
Dimash <abdygalykd@gmail.com>
2026-02-15 12:31:55 +0500

    Control flow - @for

--- lab4/learn Angular/my-first-app/src/app/app.ts
+++ lab4/learn Angular/my-first-app/src/app/app.ts
@@ -1,24 +1,18 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  template: ` Username: {{ username }} `,
-})
-export class User {
-  username = 'youngTech';
-}
-
-@Component({
   selector: 'app-root',
   template: `
-    @if (isServerRunning){
-      <span>Yes, the server is running </span>
-    }
-    @else{
-      <span>No, the server is not running </span>
-    }
-  `
+  @for (user of users; track user.id){
+  <p> {{user.name}}</p>
+  }`,
 })
 export class App {
-  isServerRunning = true;
+  users = [
+  {id: 0, name: 'Sarah'},
+  {id: 1, name: 'Amy'},
+  {id: 2, name: 'Rachel'},
+  {id: 3, name: 'Jessica'},
+  {id: 4, name: 'Poornima'},
+];
 }
